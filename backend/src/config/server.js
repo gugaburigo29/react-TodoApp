4bb0f92d20a5bc092e2dae0814720ef17f2b98af
guga <guga.burigo29@outlook.com>
2018-09-14 21:04:52 -0300

    Init

---
+++ backend/src/config/server.js
@@ -0,0 +1,16 @@
+const porta = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const allowCors = require('./cors')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(allowCors)
+
+app.listen(porta, function () {
+    console.log(`Servidor rodando na porta http://localhost:${porta}`)
+})
+
+module.exports = app
