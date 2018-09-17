13110af6c59239defbd9ac281026401ca060a2fa
Guga <guga.burigo29@outlook.com>
2018-09-17 13:02:40 -0300

    Adicionado component de grids

---
+++ frontend/src/template/grid.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class Grid extends Component {
+    toCssClasses(numbers) {
+        const cols = numbers ? numbers.split(' ') : []
+        let classes = ''
+
+        if (cols[0]) classes += `col-${cols[0]} `
+        if (cols[1]) classes += `col-sm-${cols[1]} `
+        if (cols[3]) classes += `col-md-${cols[3]} `
+        if (cols[4]) classes += `col-lg-${cols[4]} `
+        if (cols[5]) classes += `col-xl-${cols[5]} `
+
+        return classes
+    }
+
+    render() {
+        const gridClasses = this.toCssClasses(this.props.cols || 12)
+    }
+}
